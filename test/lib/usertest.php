68ee4d03d9c7a56a3aa6bf6a7d012384eebf7e98
klawd prime <klawd+git@kamundo.de>
2012-04-19 19:23:16 +0200

    Corrected license

--- test/lib/usertest.php
+++ test/lib/usertest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (C) <2012> <Michael Saller>
+ * Copyright (C) 2012 Michael Saller
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
@@ -84,5 +84,10 @@ class UserTest extends \PHPUnit_Framework_TestCase {
         catch(\enork\Exception_UserExists $e) {
             // pass
         }
+    }
+
+    public function testGetRootUserLazy() {
+        $rootUser = self::$kernel;
+        $this->assertTrue($rootUser);
     }
 }
